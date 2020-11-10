6bb06964dd0c3c30e956a0c71fa7afb6aae7fbbd
Chris LoJacono <clojacono92@gmail.com>
2020-11-10 11:46:41 -0600

    arrays of mushroom data made

---
+++ src/components/helpers/data/mushroomData.js
@@ -0,0 +1,225 @@
+const mushrooms = [
+  {
+    id: 1,
+    name: 'Button Mushrooms',
+    description:
+      'Button mushrooms are also called baby mushrooms or white mushrooms.',
+    imgUrl:
+      'https://grocycle.com/wp-content/uploads/2019/04/Growing-Button-Mushrooms-1024x512.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 2,
+    name: 'Cremini Mushrooms',
+    description:
+      'Cremini mushrooms are a brown variation with a slightly deeper flavour.',
+    imgUrl:
+      'https://grocycle.com/wp-content/uploads/2019/04/Cremini-Mushrooms-1024x512.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 3,
+    name: 'Portobello Mushrooms',
+    description:
+      'Portobello mushrooms are the final full-grown stage of the button mushroom’s life.',
+    imgUrl:
+      'https://grocycle.com/wp-content/uploads/2019/04/Portobello-Mushrooms-1024x512.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 4,
+    name: 'Oyster Mushrooms',
+    description:
+      'They offer a mild and sweet flavor and make a great transition from portobellos into more adventurous types of mushrooms.',
+    imgUrl:
+      'https://grocycle.com/wp-content/uploads/2019/04/Oyster-Mushrooms-2-1024x512.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 5,
+    name: 'King Oyster Mushrooms',
+    description:
+      'King oysters look similar to regular oyster mushrooms that are growing on top of a thick, white stem.',
+    imgUrl: 'https://gbc-cdn-public-media.azureedge.net/img80673.1426x713.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 6,
+    name: 'Chanterelle Mushrooms',
+    description:
+      'Chanterelle mushrooms have a distinctive bright yellow color. They’ve got a sweet and peppery flavor that goes well with eggs.',
+    imgUrl:
+      'https://grocycle.com/wp-content/uploads/2020/01/What-Are-Chanterelle-Mushrooms-1024x512.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 7,
+    name: 'Porcini Mushrooms',
+    description:
+      'They’ve got a distinctive taste that some people compare to sourdough bread, with slightly creamy and nutty flavors.',
+    imgUrl:
+      'https://www.thespruceeats.com/thmb/D09kU8o6YZJncPAys_V-hZr-PbI=/2579x2579/smart/filters:no_upscale()/166262945-56a539383df78cf772870c45.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 8,
+    name: 'Hedgehog Mushrooms',
+    description:
+      'Hedgehog or sweet tooth mushrooms get their name from the gills underneath their cap. They hang down, forming spiky shapes that resemble a hedgehog.',
+    imgUrl:
+      'https://themushroomforager.com/wp-content/uploads/2012/09/IMG_2131-715x1024.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 9,
+    name: 'Chicken Of The Woods Mushrooms',
+    description:
+      'This mushroom grows in clusters on the side of trees and is a beautiful orange color. Normally deep orange in the middle with a lighter orange color around the edges.',
+    imgUrl:
+      'https://images.food52.com/eeL67pkwbkji3UBbKS7oQ1W1tzU=/1200x1200/a86d698a-ed90-4314-a105-d44549a9a341--DSCF2052_half.JPG',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 10,
+    name: 'Black Trumpet Mushrooms',
+    description:
+      'They aren’t a very attractive mushroom to look at, but their flavor is quite sought after. People describe the flavor as rich and smoky. When allowed to dry, their flavor even takes on hints of black truffle.',
+    imgUrl:
+      'https://foragerchef.com/wp-content/uploads/2013/11/black-trumpet-mushrooms-1.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 11,
+    name: 'Wood Blewit Mushrooms',
+    description:
+      'Wood blewits are considered edible, although they may cause allergic reactions in some people. Especially when eaten raw, although they can cause reactions even when cooked.',
+    imgUrl:
+      'https://selbyshrooms.com.au/wp-content/uploads/2014/08/IMG_72181.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 12,
+    name: 'Morel Mushrooms',
+    description:
+      'Morels are some of the most sought after wild mushrooms. They aren’t farmed and sold in stores.',
+    imgUrl:
+      'https://grocycle.com/wp-content/uploads/2020/02/Where-Do-Morel-Mushrooms-Grow-1024x512.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 13,
+    name: 'Enoki Mushrooms',
+    description:
+      'These mushrooms are most commonly used in Asian cooking. They come in large clusters of tiny mushrooms with very long stems and small caps. ',
+    imgUrl:
+      'https://grocycle.com/wp-content/uploads/2019/04/Enoki-Mushrooms-1024x512.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 14,
+    name: 'Shimeji Mushrooms',
+    description:
+      'Shimeji is another mushroom native to East Asia, but they can also be found in Northern Europe. They are also known as white beech mushrooms. They add a rich umami flavor to dishes.',
+    imgUrl:
+      'https://www.producemarketguide.com/sites/default/files/Variety.tar/Variety/hon-shimeji-mushrooms_variety-page.png',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 15,
+    name: 'Maitake Mushrooms',
+    description:
+      'Maitake mushrooms are also called hen-of-the-woods. These mushrooms are too tough to eat once they reach any reasonable size. However, they’re used for their medicinal properties.',
+    imgUrl:
+      'https://grocycle.com/wp-content/uploads/2020/02/How-To-Grow-Maitake-Mushrooms-1024x512.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+  {
+    id: 16,
+    name: 'Death Cap Mushrooms',
+    description:
+      'Perhaps the deadliest of all mushrooms, the death cap is found throughout Europe and closely resembles edible straw mushrooms and caesar’s mushrooms. Its heat-stable amatoxins withstand cooking temperatures and quickly damage cells throughout the body.',
+    imgUrl:
+      'https://cdn.mos.cms.futurecdn.net/X8g2aJhU9fxWyqSZefTNqR.jpg',
+    isMagic: false,
+    isPoisonous: false,
+    isDeadly: true,
+  },
+  {
+    id: 17,
+    name: 'Webcaps',
+    description:
+      'These mushrooms feature a poison known as orellanin, which initially causes symptoms similar to the common flu. Orellanin has an insidiously long latency period and may take 2 days to 3 weeks to cause symptoms, often leading to a misdiagnosis. ',
+    imgUrl:
+      'https://cdn.britannica.com/s:800x1000/66/180666-050-FED69BBD/webcaps-Europe-Ingestion-mushrooms-symptoms.jpg',
+    isMagic: false,
+    isPoisonous: true,
+    isDeadly: false,
+  },
+  {
+    id: 18,
+    name: 'Autumn Skullcap',
+    description:
+      'Common throughout the Northern Hemisphere and parts of Australia, Galerina marginata is a gilled, wood-rotting mushroom with the same amatoxins as the death cap mushroom. ',
+    imgUrl:
+      'https://cdn.britannica.com/s:800x1000/03/180803-050-11B5EAE5/skullcaps-mushrooms-autumn-skullcap.jpg',
+    isMagic: false,
+    isPoisonous: true,
+    isDeadly: false,
+  },
+  {
+    id: 19,
+    name: 'Destroying Angels',
+    description:
+      'The destroying angels are actually several species of all-white mushrooms in the genus Amanita. These incredibly toxic fungi are very similar in appearance to edible button mushrooms and the meadow mushrooms, and have been collected by mistake on numerous occassions.',
+    imgUrl:
+      'https://nature.mdc.mo.gov/sites/default/files/styles/centered_full/public/media/images/2013/05/destroying_angel_05-01-13.jpg?itok=MrrNJhPW',
+    isMagic: false,
+    isPoisonous: true,
+    isDeadly: false,
+  },
+  {
+    id: 20,
+    name: 'Psilocybin mushroom',
+    description:
+      'Psilocybin mushrooms, commonly known as magic mushrooms or shrooms, are a polyphyletic, informal group of fungi that contain psilocybin and psilocin',
+    imgUrl:
+      'https://www.oregonlive.com/resizer/wpWOUnicTVv6S_vuonqKXklu7pg=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/D6Q376RIWBCKVEUVW5WSKKWA4M.jpg',
+    isMagic: true,
+    isPoisonous: false,
+    isDeadly: false,
+  },
+];
+
+const basket = [];
+
+export default { basket, mushrooms };
